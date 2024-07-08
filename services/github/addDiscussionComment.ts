c3388fa731542505952e19c2df9f775d83b04a38
Carson <carson.bentley@us.kbr.com>
2024-07-08 13:26:52 -0600

    test

--- services/github/addDiscussionComment.ts
+++ services/github/addDiscussionComment.ts
@@ -24,13 +24,13 @@ const ADD_DISCUSSION_COMMENT_QUERY = `
         bodyHTML
         reactionGroups {
           content
-          users [
+          users {
             author {
               avatarUrl
               login
               url
             }
-          ]
+          }
           viewerHasReacted
         }
         replies(first: 100) {
